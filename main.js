b345c546a4d5c14eaa0054a9b706aca443b52aeb
Luka Koridze <luka.qoridze2002@gmail.com>
2022-04-17 01:45:56 +0400

    Section 1 ( HTML,CSS,JS)

---
+++ main.js
@@ -0,0 +1,15 @@
+// First Section
+let backgroundImages=[
+    '/images/img1.png',
+    '/images/img2.png',
+    '/images/img3.png',
+    '/images/img4.png'
+]
+let index=0;
+setInterval(function(){
+    if(index===3){
+        index=-1;
+    }
+    index++;
+    document.querySelector('.section1').style.backgroundImage=`url('${backgroundImages[index]}')`
+},5000)

e55be6b59f445e340eb7778ac27ff59b1aeb4d10
Luka Koridze <luka.qoridze2002@gmail.com>
2022-04-17 01:57:43 +0400

    BugFix#3

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let backgroundImages=[
     './images/img3.png',
     './images/img4.png'
 ]
+document.querySelector('.section1').style.backgroundImage=`url('./images/img1.png')`
 let index=0;
 setInterval(function(){
     if(index===3){

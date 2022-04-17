b36b16ef1b58d7ee6dd0966fc65ac5fc790e8e49
Luka Koridze <luka.qoridze2002@gmail.com>
2022-04-17 16:01:30 +0400

    Section2 Fully Finished

--- main.js
+++ main.js
@@ -7,13 +7,32 @@ let backgroundImages=[
 ]
 document.querySelector('.section1').style.backgroundImage=`url('./images/img1.png')`
 let index=0;
-setInterval(function(){
+
+let anim1=setInterval(changeBackground,5000)
+if(screen.width<855){
+    clearInterval(anim1)
+    document.querySelector('.section1').style.backgroundImage=`none`
+}
+function changeBackground(){
     if(index===3){
         index=-1;
     }
     index++;
     document.querySelector('.section1').style.backgroundImage=`url('${backgroundImages[index]}')`
-},5000)
+}
 
 
 // Second Section
+let firstPr1=document.querySelector('.pinkdiv1')
+let firstPr2=document.querySelector('.pinkdiv2')
+let firstPr3=document.querySelector('.pinkdiv3')
+let firstPr4=document.querySelector('.pinkdiv4')
+document.addEventListener('scroll',function(){
+    console.log(window.scrollY)
+    if(window.scrollY>245){
+        firstPr1.classList.add('pinkdiv11')
+        firstPr2.classList.add('pinkdiv22')
+        firstPr3.classList.add('pinkdiv33')
+        firstPr4.classList.add('pinkdiv44')
+    }
+})
